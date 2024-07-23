712ec5aa098c909da4fd952675c02b9facaa4d06
Quinten-14 <raymaekersq@gmail.com>
2024-07-23 12:46:22 +0200

    Topic command almost fully works with all client messages
    We only need to make it so the topic view client messages get send to
    the channel in a private message to the sender. now it sends it to the
    sender but to the not in channel window

--- srcs/commands/Topic.cpp
+++ srcs/commands/Topic.cpp
@@ -11,9 +11,19 @@ Channel* findChannelByNameTopic(const std::string& channelName, std::vector<Chan
     return NULL;
 }
 
+void    changeTopic(Channel* referencedChannel, std::vector<std::string>args)
+{
+    std::string newTopicName = args[3];
+
+    for (size_t i = 4; i < args.size(); ++i)
+        newTopicName += " " +  args[i];
+
+    referencedChannel->changeTopic(newTopicName);
+}
+
 void    topicHandler(std::vector<std::string> args, std::vector<Channel>& channels, client& sender)
 {
-    if (args.size() < 3)
+    if (args.size() < 2)
     {
         std::cerr << "Error: Not enough parameters for TOPIC command." << std::endl;
         
@@ -47,6 +57,36 @@ void    topicHandler(std::vector<std::string> args, std::vector<Channel>& channe
             sender.sendMessageToClient(clientMessage);
 
             return;
+        }
+        else
+        {
+            std::cout << "Arg size: " << args.size() << std::endl;
+            if (args.size() == 2)
+            {
+                std::string currentTopic = wantedChannel->getTopic();
+                std::string clientMessage;
+
+                // this currently doesn't display because we need to send it as a private message in the channel the user is in.
+
+                if (currentTopic.empty())
+                {
+                    std::string clientMessage = ":server 331 " + sender.getNickName() + " " + wantedChannel->getName() + " :No topic is set";
+                    std::cout << clientMessage << std::endl;
+                    sender.sendMessageToClient(clientMessage);
+                    return;
+                }
+                else
+                {
+                    std::string clientMessage = ":server 332 " + sender.getNickName() + " " + wantedChannel->getName() + " :" + currentTopic;
+                    std::cout << clientMessage << std::endl;
+                    sender.sendMessageToClient(clientMessage);
+                    return;
+                }
+
+                // end
+            }
+            else
+                changeTopic(wantedChannel, args);
         }
     }
 }
