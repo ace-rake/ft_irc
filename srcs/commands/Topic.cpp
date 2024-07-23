4ed340c728aee4dbe1325f68f6ef8db3c77ccba9
Quinten-14 <raymaekersq@gmail.com>
2024-07-23 13:03:34 +0200

    Refactored the topic code a bit to make it more readable

--- srcs/commands/Topic.cpp
+++ srcs/commands/Topic.cpp
@@ -11,6 +11,16 @@ Channel* findChannelByNameTopic(const std::string& channelName, std::vector<Chan
     return NULL;
 }
 
+static Channel*    getChannel(std::vector<std::string> args, std::vector<Channel>& channels)
+{
+    std::string channelName = args[1];
+
+    if (channelName[0] != '#')
+        channelName = "#" + channelName;
+
+    return (findChannelByNameTopic(channelName, channels));
+}
+
 void    changeTopic(Channel* referencedChannel, std::vector<std::string>args)
 {
     std::string newTopicName = args[3];
@@ -21,67 +31,69 @@ void    changeTopic(Channel* referencedChannel, std::vector<std::string>args)
     referencedChannel->changeTopic(newTopicName);
 }
 
+void    topicNotEnoughParam(client& sender)
+{
+    std::cerr << "Error: Not enough parameters for TOPIC command." << std::endl;
+    
+    std::string clientMessage = ":server 416 " + sender.getNickName() + " TOPIC :Not enough parameters";
+
+    sender.sendMessageToClient(clientMessage);
+}
+
+void    topicNoChannelFound(client& sender)
+{
+    std::cerr << "Error: No channel found for TOPIC command." << std::endl;
+    sender.sendMessageToClient("TOPIC: No channel found with this name");
+}
+
+void    topicUserNotInChannel(client& sender, Channel* channel)
+{
+    std::cerr << "Error: Sender is not in the right channel for TOPIC command" << std::endl;
+
+    std::string clientMessage = ":server 442 " + sender.getNickName() + " " + channel->getName() + " :You're not on that channel";
+
+    sender.sendMessageToClient(clientMessage);
+}
+
+void    noTopicSet(client& sender, Channel* channel)
+{
+    std::string clientMessage = ":server 331 " + sender.getNickName() + " " + channel->getName() + " :No topic is set";
+    std::cout << clientMessage << std::endl;
+    sender.sendMessageToClient(clientMessage);
+}
+
+void    topicDisplay(client& sender, Channel* channel, std::string topic)
+{
+    std::string clientMessage = ":server 332 " + sender.getNickName() + " " + channel->getName() + " :" + topic;
+    std::cout << clientMessage << std::endl;
+    sender.sendMessageToClient(clientMessage);
+}
+
 void    topicHandler(std::vector<std::string> args, std::vector<Channel>& channels, client& sender)
 {
     if (args.size() < 2)
-    {
-        std::cerr << "Error: Not enough parameters for TOPIC command." << std::endl;
-        
-        std::string clientMessage = ":server 416 " + sender.getNickName() + " TOPIC :Not enough parameters";
+        return ((void)topicNotEnoughParam(sender));
 
-        sender.sendMessageToClient(clientMessage);
-        return;
-    }
+    Channel*    wantedChannel = getChannel(args, channels);
 
-    std::string channelName = args[1];
-
-    if (channelName[0] != '#')
-        channelName = "#" + channelName;
-
-    std::cout << "ChannelName: " << channelName << std::endl;
-    Channel*    wantedChannel = findChannelByNameTopic(channelName, channels);
     if (!wantedChannel)
-    {
-        std::cerr << "Error: No channel found for TOPIC command." << std::endl;
-        sender.sendMessageToClient("TOPIC: No channel found with this name");
-        return;
-    }
+        return ((void)topicNoChannelFound(sender));
     else
     {
         if (!wantedChannel->isInClientList(sender))
-        {
-            std::cerr << "Error: Sender is not in the right channel for TOPIC command" << std::endl;
-
-            std::string clientMessage = ":server 442 " + sender.getNickName() + " " + wantedChannel->getName() + " :You're not on that channel";
-
-            sender.sendMessageToClient(clientMessage);
-
-            return;
-        }
+            return ((void)topicUserNotInChannel(sender, wantedChannel));
         else
         {
-            std::cout << "Arg size: " << args.size() << std::endl;
             if (args.size() == 2)
             {
                 std::string currentTopic = wantedChannel->getTopic();
-                std::string clientMessage;
 
                 // this currently doesn't display because we need to send it as a private message in the channel the user is in.
 
                 if (currentTopic.empty())
-                {
-                    std::string clientMessage = ":server 331 " + sender.getNickName() + " " + wantedChannel->getName() + " :No topic is set";
-                    std::cout << clientMessage << std::endl;
-                    sender.sendMessageToClient(clientMessage);
-                    return;
-                }
+                    return ((void)noTopicSet(sender, wantedChannel));
                 else
-                {
-                    std::string clientMessage = ":server 332 " + sender.getNickName() + " " + wantedChannel->getName() + " :" + currentTopic;
-                    std::cout << clientMessage << std::endl;
-                    sender.sendMessageToClient(clientMessage);
-                    return;
-                }
+                    return ((void)topicDisplay(sender, wantedChannel, currentTopic));
 
                 // end
             }
