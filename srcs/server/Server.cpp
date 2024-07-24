7ec4e6e52ed9ec7b53c47295ac0199feb1d8a8f4
Victor Denissen <vdenisse@student.s19.be>
2024-07-24 14:02:58 +0200

    cleanup part2
    server to client message function now in separate file

--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -25,44 +25,6 @@ void Server::idle()
 	}
 }
 
-std::string Server::handleClientMessage(Client & client)
-{
-	char buffer[BUFFER_SIZE];
-	int clientFd = client.getFd().fd;
-	int valread = recv(clientFd, buffer, BUFFER_SIZE, 0);
-	if (valread == 0)
-	{
-		close(clientFd);
-		clientFd = -1;
-		std::cout << "Client disconnected" <<std::endl;
-	}
-	else
-	{
-		buffer[valread] = '\0';
-		std::string msg(buffer);
-		std::cout << "Received from " << clientFd << ":\t" << msg << std::endl;
-		logCommand(msg);
-		try{
-			commandHandler(msg, client);
-		}
-		catch (std::runtime_error &e)
-		{
-			std::cout << e.what();
-		}
-		return msg;
-	}
-	return ("");
-}
-
-void	Server::broadCastMsg(std::string msg)
-{
-	for (int i = 0; i < MAX_CLIENTS && _clients[i].getFd().fd != -1; ++i)
-	{
-		std::cout << "sending msg..." << std::endl;
-		_clients[i].sendMessageToClient(msg);
-	}
-
-}
 
 Client*	Server::getUser(userData field, std::string data)
 {
