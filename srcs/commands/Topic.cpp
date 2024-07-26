e2d85197f2c52a7ce9186ad73b4e107218b615ff
Quinten Raymaekers <qraymaek@student.s19.be>
2024-07-26 15:24:43 +0200

    Topic can only be changed if person has operator rights

--- srcs/commands/Topic.cpp
+++ srcs/commands/Topic.cpp
@@ -47,6 +47,15 @@ void    topicUserNotInChannel(Client& sender, Channel* channel)
     sender.sendMessageToClient(clientMessage);
 }
 
+void    topicUserNotOperator(Client& sender, Channel* channel)
+{
+    std::cerr << "Error: Sender does not have operator privileges for TOPIC command." << std::endl;
+
+    std::string clientMessage = ":server 482 " + sender.getNickName() + " " + channel->getName() + " :You're not channel operator";
+
+    sender.sendMessageToClient(clientMessage);
+}
+
 void    topicHandler(std::vector<std::string> args, std::vector<Channel> &channels, Client &sender)
 {
     Channel*    wantedChannel = getChannel(args, channels);
@@ -56,6 +65,9 @@ void    topicHandler(std::vector<std::string> args, std::vector<Channel> &channe
 
     if (wantedChannel->findClient(ID, sender.getId()) == wantedChannel->getClients().end())
         return ((void)topicUserNotInChannel(sender, wantedChannel));
+
+    if (!wantedChannel->clientIsOperator(sender))
+        return ((void)topicUserNotOperator(sender, wantedChannel));
 
     if (args.size() > 2)
         changeTopic(wantedChannel, args, sender);
