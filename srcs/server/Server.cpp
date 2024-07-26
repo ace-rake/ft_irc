728d93c0345766c2ea8a4be273cedb4d682c811a
Victor Denissen <vdenisse@student.s19.be>
2024-07-26 12:03:46 +0200

    Changed try catch block to be around entire idle block

--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -13,24 +13,22 @@ void Server::idle()
 {
 	while (1)
 	{
-		int retval = poll(_fds, MAX_CLIENTS, -1);
 
-		if (retval > 0)
+		try {
+			int retval = poll(_fds, MAX_CLIENTS, -1);
+
+			if (retval > 0)
+			{
+				if (_server.revents & POLLIN)
+					handleNewConnection();
+				for (int i = 0; i < MAX_CLIENTS; ++i)
+					if (_clients[i].getFd().fd != -1 && (_clients[i].getFd().revents & POLLIN))
+						handleClientMessage(_clients[i]);
+			}
+		}
+		catch (std::runtime_error &e)
 		{
-			if (_server.revents & POLLIN)
-				handleNewConnection();
-			for (int i = 0; i < MAX_CLIENTS; ++i)
-				if (_clients[i].getFd().fd != -1 && (_clients[i].getFd().revents & POLLIN))
-				{
-					try
-					{
-					handleClientMessage(_clients[i]);
-					}
-					catch (std::runtime_error &e)
-					{
-						std::cout << e.what() << std::endl;
-					}
-				}
+			std::cerr << e.what() << std::endl;
 		}
 	}
 }
@@ -41,15 +39,15 @@ Client*	Server::getUser(userData field, std::string data)
 	std::string (Client::*funcptr)()const;
 	switch (field){
 		case (NICK):
-		{
-			funcptr = &Client::getNickName;
-			break;
-		}
+			{
+				funcptr = &Client::getNickName;
+				break;
+			}
 		case (USER):
-		{
-			funcptr = &Client::getUserName;
-			break;
-		}
+			{
+				funcptr = &Client::getUserName;
+				break;
+			}
 		default:
 			return (NULL);
 	}
