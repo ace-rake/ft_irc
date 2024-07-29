a83dc38217fb6b2389f5ab5a84211a75d75a6477
Quinten-14 <raymaekersq@gmail.com>
2024-07-29 16:19:03 +0200

    feat:

--- srcs/channel/ChannelUtils.cpp
+++ srcs/channel/ChannelUtils.cpp
@@ -9,11 +9,12 @@ int	Channel::deleteClient(std::vector<Client>::iterator client)
 	return (0);
 }
 
-int	Channel::deleteClient(Client & c)
+int Channel::deleteClient(Client &c)
 {
-	std::vector<Client>::iterator it = findClient(USER, c.getUserName());
-	if (it != _clients.end())
-    {   
+    std::vector<Client>::iterator it = findClient(USER, c.getUserName());
+    if (it != _clients.end())
+    {
+        // Prepare the part message before removing the client.
         std::string partMessage = ":";
         partMessage += c.getNickName() + "!";
         partMessage += c.getUserName() + "@";
@@ -21,13 +22,17 @@ int	Channel::deleteClient(Client & c)
         partMessage += " PART ";
         partMessage += getName();
         partMessage += " :Reason";
+
+        // Remove the client first.
+        _clients.erase(it);
+        _settings.userAmount--;
+
+        // Then broadcast the part message to the remaining clients.
         broadcastMsg(partMessage);
 
-		deleteClient(it);
-	    _settings.userAmount--;
-	    // TODO: check if last user has left
+        // TODO: check if last user has left and perform necessary cleanup.
     }
-	return (0);
+    return (0);
 }
 
 void    Channel::kickUser(Client sender, std::string victimName, std::vector<std::string>args)
