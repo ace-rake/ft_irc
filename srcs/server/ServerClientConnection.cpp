951028206b1ed33e81dcef60be7ca107b4ec6621
Victor Denissen <vdenisse@student.s19.be>
2024-08-08 14:49:04 +0200

    serverClientConnection commented some unused functions, updated clearClient func

--- srcs/server/ServerClientConnection.cpp
+++ srcs/server/ServerClientConnection.cpp
@@ -20,72 +20,75 @@ void Server::handleNewConnection()
 	send(new_connection, msg.c_str(), msg.size(), 0);
 }
 
-std::string	Server::receiveUserData(struct pollfd client)
-{
-	std::string buffer;
-	std::string str;
-	bool user_received = false;
+/* std::string	Server::receiveUserData(struct pollfd client) */
+/* { */
+/* 	std::string buffer; */
+/* 	std::string str; */
+/* 	bool user_received = false; */
+/*  */
+/* 	while (!user_received) { */
+/* 		buffer += readUserData(client.fd); */
+/*  */
+/* 		size_t pos; */
+/* 		while ((pos = buffer.find("\r\n")) != std::string::npos) { */
+/* 			str += buffer.substr(0, pos); // Extract the complete message */
+/* 			str += " "; */
+/* 			buffer.erase(0, pos + 2); // Remove the processed message */
+/*  */
+/* 			std::cout << "Current message: " << str << std::endl; */
+/*  */
+/* 			if (str.find("USER") != std::string::npos) { */
+/* 				user_received = true; */
+/* 				break; */
+/* 			} */
+/* 		} */
+/*  */
+/* 		if (user_received) { */
+/* 			std::cout << "Full USER command received: " << str << std::endl; */
+/* 		} */
+/* 	} */
+/* 	return str; */
+/* } */
+/* std::string Server::readUserData(int &fd) */
+/* { */
+/* 	char buffer[BUFFER_SIZE]; */
+/* 	int valread = recv(fd, buffer, BUFFER_SIZE, 0); */
+/* 	if (valread == 0) */
+/* 	{ */
+/* 		close(fd); */
+/* 		fd = -1; */
+/* 		std::cout << "Client disconnected" <<std::endl; */
+/* 	} */
+/* 	else */
+/* 	{ */
+/* 		buffer[valread] = '\0'; */
+/* 		std::string msg(buffer); */
+/* 		return msg; */
+/* 	} */
+/* 	return (""); */
+/* } */
 
-	while (!user_received) {
-		buffer += readUserData(client.fd);
-
-		size_t pos;
-		while ((pos = buffer.find("\r\n")) != std::string::npos) {
-			str += buffer.substr(0, pos); // Extract the complete message
-			str += " ";
-			buffer.erase(0, pos + 2); // Remove the processed message
-
-			std::cout << "Current message: " << str << std::endl;
-
-			if (str.find("USER") != std::string::npos) {
-				user_received = true;
-				break;
-			}
-		}
-
-		if (user_received) {
-			std::cout << "Full USER command received: " << str << std::endl;
-		}
-	}
-	return str;
-}
-std::string Server::readUserData(int &fd)
-{
-	char buffer[BUFFER_SIZE];
-	int valread = recv(fd, buffer, BUFFER_SIZE, 0);
-	if (valread == 0)
-	{
-		close(fd);
-		fd = -1;
-		std::cout << "Client disconnected" <<std::endl;
-	}
-	else
-	{
-		buffer[valread] = '\0';
-		std::string msg(buffer);
-		return msg;
-	}
-	return ("");
-}
-
-int	Server::requestPsw(Client & client)
-{
-	if (_serverPassword.empty())
-		return (0);
-	std::string msg = "NOTICE AUTH :*** Password required";
-	client.sendMessageToClient(msg);
-	return (0);
-}
+/* int	Server::requestPsw(Client & client) */
+/* { */
+/* 	if (_serverPassword.empty()) */
+/* 		return (0); */
+/* 	std::string msg = "NOTICE AUTH :*** Password required"; */
+/* 	client.sendMessageToClient(msg); */
+/* 	return (0); */
+/* } */
 
 void	Server::clearClient(Client & client)
 {
-	std::vector<std::string>::iterator it = client.getChannels().begin();
-	for (;it != client.getChannels().end(); ++it)
+	std::vector<std::string> channels = client.getChannels();
+	int size = channels.size();
+	std::cout << "Size :" << size << std::endl;
+	for (int i = 0; i != size; ++i)
 	{
-		Channel * channel = getChannel(*it);
+		Channel * channel = getChannel(channels[i]);
 		if (!channel)
 			throw std::runtime_error("Server::clearClient: Error: client is in nonexistent channel");
 		channel->removeIdFromList(client.getId());
+		channel->removeClientOperator(client);
 		channel->deleteClient(client);
 	}
 	client.clear();
